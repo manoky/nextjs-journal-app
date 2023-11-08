c9b80cb7223d1201a88297b93c5fc03caa1b6afc
Emmanuel <emmopk11@outlook.com>
2023-11-08 23:44:08 +0100

    Initial commit
    Added clerk with middleware for authentication
    
    Signed-off-by: Emmanuel <emmopk11@outlook.com>

---
+++ src/app/journal/page.tsx
@@ -0,0 +1,13 @@
+import { SignOutButton } from '@clerk/nextjs'
+const JournalPage = () => {
+  return (
+    <div>
+      Journals!!
+      <div>
+        <SignOutButton />
+      </div>
+    </div>
+  )
+}
+
+export default JournalPage
